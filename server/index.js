e3c259e6d1b7b831c8ee8c7df65595b2817cf0a5
Tyler Collier <tyler.collier+github@devmounta.in>
2018-05-22 11:49:39 -0700

    Done with Step 1

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('express-session');
+require('dotenv').config();
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 10000,
+  },
+}));
 
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
