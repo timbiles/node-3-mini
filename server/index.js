642f3053682710824acc99f0e61e76d55166d5a6
Tim Biles <timbilestimbiles@gmail.com>
2018-07-10 12:54:04 -0500

    initial setup

--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mc = require( `./controllers/messages_controller` );
+const mc = require(`./controllers/messages_controller`);
+const session = require('express-session');
 const port = 1337;
+
+const createInitialSession = require('./middlewares/session');
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../build`));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 10000 }
+  })
+);
 
-app.post( "/api/messages", mc.create );
-app.get( "/api/messages", mc.read );
-app.put( "/api/messages", mc.update );
-app.delete( "/api/messages", mc.delete );
+app.use(createInitialSession)
 
+app.post('/api/messages', mc.create);
+app.get('/api/messages', mc.read);
+app.put('/api/messages', mc.update);
+app.delete('/api/messages', mc.delete);
 
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
